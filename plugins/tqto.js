d752b57a0e3b553ba46ca8a9e06e41cdb714b34d
Fanz1234 <duffanblank@gmail.com>
2023-06-27 11:52:49 +0700

    FanzMdV5

---
+++ plugins/tqto.js
@@ -0,0 +1,43 @@
+let fetch = require('node-fetch')
+
+let handler = async (m) => {
+    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/f38f1ce61e1e674e20e94.png')
+    //let wibu = `https://api.zacros.my.id/randomimg/loli`
+    let res = await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/neko.txt')
+    let txt = await res.text()
+    let arr = txt.split('\n')
+    let zeen = arr[Math.floor(Math.random() * arr.length)]
+    let thumb = await(await fetch(zeen)).buffer()
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    else who = m.sender
+    let user = global.db.data.users[who]
+let tqto = `*▸ - - - —「 BIG THANKS TO 」— - - - ◂*
+*❉ Adiwajshing*
+*❉ Bochil Gaming*
+*❉ BrunoSobrino*
+*❉ Jarot Offc*
+*❉ Kriz offc*
+*❉ FanzOfc*
+*❉ Mursid*
+
+*▸ - - - —「 Script 」— - - - ◂*
+*❉ chat owner *
+
+`
+conn.sendButtonDoc(m.chat, tqto, wm,'Thanks','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "-",
+    mediaType: "VIDEO",
+    description: "-", 
+    title: 'Fanz-MultiDevice',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
+}
+} })
+        }
+handler.help = ['tqto']
+handler.tags = ['main','info']
+handler.command = /^(credits|credit|thanks|thanksto|tqto)$/i
+
+module.exports = handler
